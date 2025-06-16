49e5a0a3dffcab7332ca5e3df635a471e3a157c3
apdlvmf8087 <apdlvmf8087@naver.com>
2025-06-16 22:00:23 +0900

    hp 이모지

--- Assets/Scripts/Manager/SharedHpManager.cs
+++ Assets/Scripts/Manager/SharedHpManager.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 public class SharedHpManager : MonoBehaviour
 {
     public HPBar hpBar;
-    public HPText hpText;
 
     public void UpdateHPBar(float currentHp, float maxHp)
     {
         hpBar?.UpdateHP(currentHp, maxHp);
-        hpText?.UpdateHP(currentHp, maxHp);
     }
 }
