9e221448bd531c17320fa84227af1bf82c7f52c1
Acasdex <changju716@naver.com>
2025-06-17 17:22:08 +0900

    HP 퍼센트에 따라 이모지 자동 변경 기능 추가

--- Assets/Scripts/UI/HPBar.cs
+++ Assets/Scripts/UI/HPBar.cs
@@ -3,7 +3,15 @@ using UnityEngine.UI;
 
 public class HPBar : MonoBehaviour
 {
+    [Header("HP Bar")]
     Image hpImg;
+    
+    [Header("HP Emoji")]
+    public Image emojiImage; 
+    public Sprite hp100Emoji; 
+    public Sprite hp50Emoji;  
+    public Sprite hp0Emoji;   
+    
     void Awake()
     {
         hpImg = GetComponent<Image>();
@@ -12,7 +20,32 @@ public class HPBar : MonoBehaviour
     public void UpdateHP(float currentHP, float maxHP)
     {
         if (!hpImg) return;
+        
         float healthPercent = Mathf.Clamp01(currentHP / maxHP);
         hpImg.fillAmount = healthPercent;
+        
+        UpdateEmoji(currentHP);
+    }
+    
+    void UpdateEmoji(float currentHP)
+    {
+        if (!emojiImage) return;
+        
+        // HP 값에 따라 이모지 변경 
+        if (currentHP > 66f) 
+        {
+            emojiImage.sprite = hp100Emoji;
+        }
+        else if (currentHP > 33f)
+        {
+            emojiImage.sprite = hp50Emoji;
+        }
+        else 
+        {
+            emojiImage.sprite = hp0Emoji;
+        }
+        
+        // 이모지 표시
+        emojiImage.gameObject.SetActive(true);
     }
 }
