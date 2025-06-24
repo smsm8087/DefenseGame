3b929385e6b09430e5d288207272962d9474dc2b
apdlvmf8087 <apdlvmf8087@naver.com>
2025-06-24 18:31:44 +0900

    텍스트 수정

--- Assets/Scripts/UI/CountDownText.cs
+++ Assets/Scripts/UI/CountDownText.cs
@@ -38,16 +38,17 @@ namespace UI
         public IEnumerator DurationCoroutin(int duration, List<CardData> cards)
         {
             float elapsedTime = 0;
-            while (elapsedTime < duration * 1000)
+            while (elapsedTime < duration)
             {
                 elapsedTime++;
                 centerText.text = $"카드선택까지 남은시간 : {(duration - elapsedTime).ToString()}\n " +
                                   $"카드 : {TextManager.Instance.GetText(cards[0].title)}{cards[0].value}%\n" +
                                   $"카드 : {TextManager.Instance.GetText(cards[1].title)}{cards[1].value}%\n" +
                                   $"카드 : {TextManager.Instance.GetText(cards[2].title)}{cards[2].value}%\n";
+                centerText.fontSize = 15;
                 yield return new WaitForSeconds(1);
             }
-            gameObject.SetActive(duration > 0);
+            gameObject.SetActive(false);
         }
     }
 }
