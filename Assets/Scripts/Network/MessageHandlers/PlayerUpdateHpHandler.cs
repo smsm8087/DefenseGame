8381b717e7e86c109c7dcc57278c0b6f2bb5253d
apdlvmf8087 <apdlvmf8087@naver.com>
2025-07-03 16:05:31 +0900

    플레이어 hp update handler 추가

---
+++ Assets/Scripts/Network/MessageHandlers/PlayerUpdateHpHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerUpdateHpHandler : INetworkMessageHandler
+{
+    public string Type => "player_update_hp";
+    private readonly ProfileUI profileUI;
+
+    public PlayerUpdateHpHandler(ProfileUI profileUI)
+    {
+        this.profileUI = profileUI;
+    }
+    public void Handle(NetMsg msg)
+    {
+        //프로필 ui 업데이트
+        profileUI.UpdateHp(msg.playerInfo.currentHp, msg.playerInfo.currentMaxHp);
+    }
+}
